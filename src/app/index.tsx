2d2321e6d08d6b4fc4f4c21bc9ec4c67909b0a7e
sistema dyan480 <dayanavillamizar480@gmail.com>
2026-09-05 17:04:58 -0500

    Actualizacion de index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -43,6 +43,10 @@ export default function HomeScreen() {
           get started
         </ThemedText>
 
+        <ThemedText>
+          gholaaaaaaaaaaaaaaaa
+        </ThemedText>
+
         <ThemedView type="backgroundElement" style={styles.stepContainer}>
           <HintRow
             title="Try editing"
